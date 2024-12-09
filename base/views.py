9746755e5fa6e239e82c15092d0a5eaf77424345
Collins Korir <kibetkorir75@gmail.com>
2024-12-10 00:05:56 +0300

    second commit

--- base/views.py
+++ base/views.py
@@ -3,18 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 rooms=[
-    {'id':1, 'name':'lets learn python'},
+    {'id':1, 'name':'let\s learn python'},
     {'id':2, 'name':'Design with me'},
     {'id':3, 'name':'Frontend developers'},
 ]
 
-
-
-
-
 def home(request):
     context={'rooms':rooms}
-    return render(request, 'home.html',context)
+    return render(request, 'base/home.html',context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for r in rooms:
+        if r['id'] == int(pk):
+            room = r
+            break
+
+    if room:
+        context = {'room': room}
+        return render(request, 'base/room.html', context)
+    else:
+        # Handle the case where the room is not found
+        return render(request, '404.html', status=404)
