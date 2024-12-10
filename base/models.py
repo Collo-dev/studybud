08da5e6f0f244547db96da2943c8c2b502bd784c
Collins Korir <kibetkorir75@gmail.com>
2024-12-10 20:07:04 +0300

    third commit

--- base/models.py
+++ base/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.model import User
+
+
+class Topic(models.Model):
+    name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
+class Room(models.Model):
+    host=user=models.ForeignKey(User,on_delete=model.CASCADE)
+    topic=models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name= models.CharField(max_length=200)
+    description=models.TextField(null=True, blank=True)
+    #participants=
+    updated= models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user=models.ForeignKey(User,on_delete=model.CASCADE)
+    room=models.ForeignKey(Room, on_delete=models.CASCADE)
+    body=models.textField()
+    updated=models.DateTimeField(auto_now=True)
+    created=DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,27 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
-rooms=[
-    {'id':1, 'name':'let\s learn python'},
-    {'id':2, 'name':'Design with me'},
-    {'id':3, 'name':'Frontend developers'},
-]
+# rooms=[
+#     {'id':1, 'name':'let\s learn python'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developers'},
+# ]
 
 def home(request):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(request, 'base/home.html',context)
 
-def room(request, pk):
-    room = None
-    for r in rooms:
-        if r['id'] == int(pk):
-            room = r
-            break
 
-    if room:
-        context = {'room': room}
-        return render(request, 'base/room.html', context)
-    else:
-        # Handle the case where the room is not found
-        return render(request, '404.html', status=404)
+
+def room(request, pk):
+    room=Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+    
